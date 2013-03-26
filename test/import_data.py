a2370328a4fe23e8d6a56c4fe281d95c443c1ca4
Gregory Ditzler <gditzler@Gregorys-MacBook-Pro.local>
2013-03-26 16:01:53 -0400

    cast ints as floats in import_data.py

--- test/import_data.py
+++ test/import_data.py
@@ -37,7 +37,7 @@ def uniform_data(n_observations = 1000, n_features = 50, n_relevant = 5):
 	import numpy as np
 	xmax = 10
 	xmin = 0
-	data = np.random.randint(xmax + 1, size = (n_features, n_observations))
+	data = 1.0*np.random.randint(xmax + 1, size = (n_features, n_observations))
 	labels = np.zeros(n_observations)
 	delta = n_relevant * (xmax - xmin) / 2.0
 
